f1df50007e35760dac4f1f689badff57ff4637a4
챙 <cindy03@tukorea.ac.kr>
2025-04-08 14:47:23 +0900

    update0407

--- ch02/polynominal.c
+++ ch02/polynominal.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
-
 #define MAX_DEGREE 1001
 
 typedef struct
 {
-    int degree;
-    float coef[MAX_DEGREE];
+    int degree; //다항식의 차수
+    float coef[MAX_DEGREE]; // 계수의 배열 (지수가 0일 때부터 입력)
 } Polynominal;
 
-int degree (Polynominal p)
+int degree (Polynominal p) // 다항식 p의 차수 반환
 {
     return p.degree;
 }
 
-float coefficient (Polynominal p, int i)
+float coefficient (Polynominal p, int i) // p의 지수가 i인 항의 계수 반환 -> x^i항의 계수 반환
 {
     return p.coef[i];
 }
 
-float evaluate (Polynominal p, float x)
+float evaluate (Polynominal p, float x) //
 {
-    float result = p.coef[0];
-    float mul = 1;
+    float result = p.coef[0]; // result에 상수항부터 더함
+    float mul = 1; // 초깃값 (x^i 표기 위함)
     for (int i = 1; i <= p.degree; i++) 
     {
-        mul *= x;
-        result += p.coef[i] * mul;
+        mul *= x; // 계속 곱해주면서 x^i 포현
+        result += p.coef[i] * mul; // x^i * 계수 를 result에 추가로 더함
     }
 
     return result;
@@ -34,24 +33,24 @@ float evaluate (Polynominal p, float x)
 Polynominal add (Polynominal a, Polynominal b)
 {
     Polynominal p;
-    p.degree = (a.degree > b.degree) ? a.degree : b.degree;
+    p.degree = (a.degree > b.degree) ? a.degree : b.degree; // a와 b 중 차수가 큰 쪽이 결과 다항식의 차수
 
     for (int i = 0; i < p.degree; i++)
     {
-        p.coef[i] = ((i <= a.degree) ? a.coef[i] : 0) + ((i <= b.degree) ? b.coef[i] : 0);
+        p.coef[i] = ((i <= a.degree) ? a.coef[i] : 0) + ((i <= b.degree) ? b.coef[i] : 0); // p의 현재 차수가 a의 차수 이하이면 a의 차수를, 아니면 0을 더함
     }
 
-    return p;
+    return p; // 결과 구조체 p (차수, 계수 배열 형태) 반환
 }
 
 void print_poly (Polynominal p, char str[])
 {
-    printf(" %s", str);
+    printf("%s", str);
     for (int i = p.degree; i > 0; i--)
     {
-        printf("%5.1f x^%d + ", p.coef[i], i);
+        printf("%5.1f x^%d + ", p.coef[i], i); // 계수x^차수+ 출력 (i = 1 까지)
     }
-    printf("%4.1f\n", p.coef[0]);
+    printf("%4.1f\n", p.coef[0]); // 계수 배열의 0번째 (= 차수가 0인 부분 = 상수항) 출력
 }
 
 int main ()
--- ch02/sparse_poly.c
+++ ch02/sparse_poly.c
@@ -1,45 +1,45 @@
 #include <stdio.h>
 #include <math.h>
-
 #define MAX_TERMS 80
 
-typedef struct
+typedef struct // 하나의 항
 {
-    int expo;
-    float coef;
+    int expo; // 항의 지수
+    float coef; // 항의 계수
 } Term;
 
-typedef struct {
-    int nTerms;
-    Term term[MAX_TERMS];
+typedef struct { // 희소 다항식 (계수가 0이 아닌 항들만)
+    int nTerms; // 계수가 0이 아닌 항의 개수
+    Term term[MAX_TERMS]; // 계수가 0이 아닌 항의 배열
 } SparsePoly;
 
-float evaluate (SparsePoly a, float x)
+float evaluate (SparsePoly a, float x) // 미지수 x에 값을 대입했을 때 결과를 반환하는 함수
 {
     float result = 0;
     for (int i = 0; i <a.nTerms; i++)
     {
         result += (float)(a.term[i].coef * pow(x, a.term[i].expo));
+        // result에 항의 값 (계수 * x의 지수승)을 누적 덧셈 (pow는 거듭제곱을 계산하는 함수 pow(2,3)은 2의 3승)
     }
 
     return result;
 }
 
-SparsePoly add (SparsePoly a, SparsePoly b)
+SparsePoly add (SparsePoly a, SparsePoly b) // 두 희소 다항식을 더하는 함수
 {
     SparsePoly c;
-    c.nTerms = 0;
-    for (int i = 0, j = 0; i < a.nTerms || j < b.nTerms;)
+    c.nTerms = 0; // c의 항 개수 초기화
+    for (int i = 0, j = 0; i < a.nTerms || j < b.nTerms;) // a 나 b 둘 중 하나라도 남아있을 때 반복
     {
-        if (i == a.nTerms || a.term[i].expo < b.term[j].expo)
+        if (i == a.nTerms || a.term[i].expo < b.term[j].expo) // a의 항 개수가 0이거나 a의 차수 < b의 차수
         {
-            c.term[c.nTerms++] = b.term[j++];
+            c.term[c.nTerms++] = b.term[j++]; // b의 항을 c에 추가
         }
-        else if (j == b.nTerms || a.term[i].expo > b.term[j].expo)
+        else if (j == b.nTerms || a.term[i].expo > b.term[j].expo) // b의 항이 더이상 없거나 a의 지수가 더 크면
         {
-            c.term[c.nTerms++] = a.term[i++];
+            c.term[c.nTerms++] = a.term[i++]; // a의 항을 c에 추가
         }
-        else
+        else // a와 b 지수가 같은 경우 (차수가 같은 경우) 계수끼리 더해서 하나의 항으로 만듦
         {
             c.term[c.nTerms] = a.term[i++];
             c.term[c.nTerms++].coef += b.term[j++].coef;
@@ -49,33 +49,33 @@ SparsePoly add (SparsePoly a, SparsePoly b)
     return c;
 }
 
-void print_spoly (SparsePoly p, char str[])
+void print_spoly (SparsePoly p, char str[]) // 다항식을 출력하는 함수
 {
     printf("%s", str);
     for (int i = 0; i <p.nTerms; i++)
     {
-        printf("%5.1f", p.term[i].coef);
-        if (p.term[i].expo > 0)
+        printf("%5.1f", p.term[i].coef); // 계수 출력
+        if (p.term[i].expo > 0) // 지수가 0보다 크면
         {
-            printf(" x^%d ", p.term[i].expo);
-            if (i != p.nTerms - 1) printf("+ ");
+            printf(" x^%d ", p.term[i].expo); // x^지수 형식 출력
+            if (i != p.nTerms - 1) printf("+ "); // 마지막 항이 아니면 + 출력
         }
     }
     printf("\n");
 }
 
-int main ()
+int main (void)
 {   
-    SparsePoly a = { 2, {{100, 10.0f}, {0, 6.0f}} };
-    SparsePoly b = { 2, {{50, 1.1f}, {3, 2.0f}} };
+    SparsePoly a = { 2, {{100, 10.0f}, {0, 6.0f}} }; // 항 2개, {x의 100승, 계수 10}, {상수항, 계수 6} 즉 10.0x^100 + 6.0
+    SparsePoly b = { 2, {{50, 1.1f}, {3, 2.0f}} }; // 1.1x^50 + 2.0x^3
     SparsePoly c = add(a, b);
 
     print_spoly(a, " A = ");
     print_spoly(b, " B = ");
     print_spoly(c, " A + B = ");
 
-    printf("A(1)= %f\n", evaluate(a, 1.0f));
-    printf("B(2)= %f\n", evaluate(b, 2.0f));
+    printf(" A(1) = %f\n", evaluate(a, 1.0f));
+    printf(" B(2) = %f\n", evaluate(b, 2.0f));
 
     return 0;
 }

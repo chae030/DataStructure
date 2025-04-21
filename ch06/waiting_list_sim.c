ff46d3bc08d472dd610bb78ff8e699726c1f9183
챙 <cindy03@tukorea.ac.kr>
2025-04-21 15:31:57 +0900

    update0420

--- ch06/waiting_list_sim.c
+++ ch06/waiting_list_sim.c
@@ -9,9 +9,9 @@ struct Waiting {
 typedef struct Waiting Element;
 #include "DbLinkedList.h"
 
-void reserve (int nperson, const char info[])
+void reserve (int nperson, const char info[]) // const 변경 불가
 {
-    static int id = 0;
+    static int id = 0; // static 함수 호출 시에 값 유지
     Element e;
     e.id = ++id;
     e.nperson = nperson;
@@ -23,5 +23,55 @@ void reserve (int nperson, const char info[])
 void find (int wid)
 {
     int nTeam = 0, nPeople = 0;
-    // 234p 보고 마저 치기~~
+    
+}
+
+void cancel(int wid)
+{
+
+}
+
+void delay(int wid)
+{
+
+}
+
+void print_waiting(void)
+{
+
+}
+
+void service(void)
+{
+
+}
+
+int main(void)
+{
+    init_list();
+    print_waiting();
+
+    reserve(2, "010-xxxx-8762"); // 웨이팅 등록 (인원, 전화번호)
+    reserve(2, "010-xxxx-3303");
+    reserve(5, "010-xxxx-1212");
+    reserve(1, "010-xxxx-2305");
+    print_waiting();
+
+    service(); // 첫번째 팀 입장
+    print_waiting();
+
+    reserve(3, "010-xxxx-4949");
+    reserve(4, "010-xxxx-7345");
+    print_waiting();
+
+    find(4); // 고객 4가 자신의 순서 확인
+    delay(3); 
+    delay(3); // 고객의 순서를 2번 뒤로 밀기
+    print_waiting();
+    cancel(5); // 고객 5번이 대기 취소
+    print_waiting();
+
+    destroy_list();
+
+    return 0;
 }
